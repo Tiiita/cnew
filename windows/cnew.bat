47c1767651e93be53f8e447d3b5803f23166bc5d
Tiiita <96084154+Tiiita@users.noreply.github.com>
2025-04-08 17:41:06 +0200

    Fix win CMake recode

--- windows/cnew.bat
+++ windows/cnew.bat
@@ -1,37 +1,37 @@
 @echo off
-setlocal
-
-if "%1"=="" (
-    echo (v1.0.0) Usage: cnew <project_name>
+if "%~1"=="" (
+    echo ^(v1.0.0^) Usage^: cnew ^<project_name^>
     exit /b 1
 )
 
-set PROJECT_NAME=%1
-set PROJECT_DIR=%cd%\%PROJECT_NAME%
+set "NAME=%~1"
+set "DIR=%cd%\%NAME%"
 
-mkdir "%PROJECT_DIR%\src"
-mkdir "%PROJECT_DIR%\build"
+mkdir "%DIR%\src" 2>nul
+mkdir "%DIR%\build" 2>nul
 
-echo /build > "%PROJECT_DIR%\.gitignore"
+> "%DIR%\.gitignore" echo /build
 
-echo #include <stdio.h> > "%PROJECT_DIR%\src\main.c"
-echo. >> "%PROJECT_DIR%\src\main.c"
-echo int main() { >> "%PROJECT_DIR%\src\main.c"
-echo     printf("Hello, World!^\\n"); >> "%PROJECT_DIR%\src\main.c"
-echo     return 0; >> "%PROJECT_DIR%\src\main.c"
-echo } >> "%PROJECT_DIR%\src\main.c"
+> "%DIR%\src\main.c" (
+    echo #include ^<stdio.h^>
+    echo.
+    echo int main^(^) {
+    echo     printf^("Hello, World!\n"^);
+    echo     return 0;
+    echo }
+)
 
-echo cmake_minimum_required(VERSION 3.10) > "%PROJECT_DIR%\CMakeLists.txt"
-echo. >> "%PROJECT_DIR%\CMakeLists.txt"
-echo project(%PROJECT_NAME% VERSION 1.0 LANGUAGES C) >> "%PROJECT_DIR%\CMakeLists.txt"
-echo. >> "%PROJECT_DIR%\CMakeLists.txt"
-echo set(CMAKE_BINARY_DIR \${CMAKE_SOURCE_DIR}/build) >> "%PROJECT_DIR%\CMakeLists.txt"
-echo. >> "%PROJECT_DIR%\CMakeLists.txt"
-echo file(GLOB SOURCES "src/*.c") >> "%PROJECT_DIR%\CMakeLists.txt"
-echo. >> "%PROJECT_DIR%\CMakeLists.txt"
-echo add_executable(%PROJECT_NAME% \${SOURCES}) >> "%PROJECT_DIR%\CMakeLists.txt"
+> "%DIR%\CMakeLists.txt" (
+    echo cmake_minimum_required^(VERSION 3.10^)
+    echo.
+    echo project^(%NAME% VERSION 1.0 LANGUAGES C^)
+    echo.
+    echo set^(CMAKE_BINARY_DIR $^{CMAKE_SOURCE_DIR^}/build^)
+    echo.
+    echo file^(GLOB SOURCES "src/*.c"^)
+    echo.
+    echo add_executable^(%NAME% ${SOURCES}^)
+)
 
-echo Project '%PROJECT_NAME%' created in %PROJECT_DIR%
+echo Project '%NAME%' created in %DIR%
 echo You can now build your project using CMake!
-
-endlocal
